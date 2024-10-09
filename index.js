e38fb98d4f7eccc5812c4e19e4955cede6a7fe22
Hossain-Md-Shahriar <hasibshahriar1@gmail.com>
2024-10-10 01:32:06 +0600

    get API created to find requests of a specific user

--- index.js
+++ index.js
@@ -103,6 +103,14 @@ async function run() {
       res.send(result);
     });
 
+    // get all volunteer requests requested by a specific user
+    app.get("/volunteer-requests/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = { "volunteer.email": email };
+        const result = await allVolunteerRequest.find(query).toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
